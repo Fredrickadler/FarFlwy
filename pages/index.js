8b34263758a619e0d5e74db088db423202bfbc45
Fredrickadler <13fredrick2001@gmail.com>
2025-09-30 15:44:08 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,19 +11,39 @@ export default function Home() {
     <>
       <Head>
         <title>Flwy MiniApp</title>
+
+        {/* MiniApp embed */}
         <meta
           name="fc:miniapp"
-          content='{"version":"1","imageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","button":{"title":"🚀 Open Flwy","action":{"type":"launch_miniapp","url":"https://far-flwy.vercel.app","splashImageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","splashBackgroundColor":"#eeccff"}}}'
+          content='{"version":"1","imageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","buttons":[{"title":"🚀 Open Flwy","action":{"type":"launch_miniapp","url":"https://far-flwy.vercel.app","splashImageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","splashBackgroundColor":"#eeccff"}}]}'
         />
+
+        {/* Backward compatibility */}
         <meta
           name="fc:frame"
-          content='{"version":"1","imageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","button":{"title":"🚀 Open Flwy","action":{"type":"launch_frame","url":"https://far-flwy.vercel.app","splashImageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","splashBackgroundColor":"#eeccff"}}}'
+          content='{"version":"1","imageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","buttons":[{"title":"🚀 Open Flwy","action":{"type":"launch_frame","url":"https://far-flwy.vercel.app","splashImageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","splashBackgroundColor":"#eeccff"}}]}'
         />
       </Head>
 
       <main style={{ textAlign: "center", padding: "50px" }}>
         <h1>Welcome to Flwy 🚀</h1>
-        <p>Your MiniApp is running inside Farcaster.</p>
+        <p>Your Farcaster MiniApp is live with an Open button.</p>
+        <button
+          onClick={() =>
+            sdk.actions.openUrl({ url: "https://example.com" })
+          }
+          style={{
+            marginTop: "20px",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "6px",
+            background: "#6c5ce7",
+            color: "#fff",
+            cursor: "pointer"
+          }}
+        >
+          Test External Link
+        </button>
       </main>
     </>
   );
