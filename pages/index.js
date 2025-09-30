663d6275f954df50ce1bfc9705474ae9e30e635b
Fredrickadler <13fredrick2001@gmail.com>
2025-09-30 15:58:36 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@
 import Head from "next/head";
 
 export default function Home() {
+  // embed JSON (using new schema: image + button)
   const miniappJson =
-    '{"version":"1","imageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","button":{"title":"🚀 Open Flwy","action":{"type":"launch_miniapp","name":"Flwy","url":"https://far-flwy.vercel.app","splashImageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","splashBackgroundColor":"#eeccff"}}}';
+    '{"version":"1","image":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","button":{"title":"🚀 Open Flwy","action":{"type":"launch_miniapp","name":"Flwy","url":"https://far-flwy.vercel.app","splashImageUrl":"https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg","splashBackgroundColor":"#eeccff"}}}';
 
   return (
     <>
@@ -11,19 +12,22 @@ export default function Home() {
         <title>Flwy MiniApp</title>
         <meta name="description" content="Flwy MiniApp" />
 
-        {/* OpenGraph for preview */}
+        {/* OpenGraph (preview image when shared) */}
         <meta property="og:title" content="Flwy MiniApp" />
-        <meta property="og:description" content="Open Flwy" />
-        <meta property="og:image" content="https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg" />
+        <meta property="og:description" content="Open Flwy MiniApp" />
+        <meta
+          property="og:image"
+          content="https://i.postimg.cc/FrqNxpv6/3011-B096-760-E-4-A33-BD0-C-3-B4-B89142-F99.jpg"
+        />
 
-        {/* Farcaster miniapp embeds */}
+        {/* Farcaster MiniApp embed (new schema) */}
         <meta name="fc:miniapp" content={miniappJson} />
         <meta name="fc:frame" content={miniappJson} />
       </Head>
 
       <main style={{ textAlign: "center", padding: "50px" }}>
         <h1>Welcome to Flwy 🚀</h1>
-        <p>This page should render a MiniApp with an Open button inside Farcaster.</p>
+        <p>This is the Flwy MiniApp frame test.</p>
       </main>
     </>
   );
